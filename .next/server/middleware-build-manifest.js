af5db05e9a13d6b984c6811c92fae23d1bca44c1
J. Erik Thompson <j.erik.thompson@ryan.com>
2025-04-15 19:01:13 -0500

    npm build

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8b95e9d75d0634bc.js","static/chunks/fd9d1056-26aaaccace7e58bc.js","static/chunks/117-cd209e2dc7412b57.js","static/chunks/main-app-76e37aa4b1ed917f.js"],pages:{"/_app":["static/chunks/webpack-8b95e9d75d0634bc.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-cc20da10e4bbeae4.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-8b95e9d75d0634bc.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-cc20da10e4bbeae4.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
